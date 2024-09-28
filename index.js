3d48613c84489f62adf7dc96d44774fbb1f72add
SumitRodrigues <sumitrod11@gmail.com>
2024-09-28 12:37:45 -0700

    implemented function currying using binding method

--- index.js
+++ index.js
@@ -29,25 +29,47 @@
 
 //Polyfill for bind method:
 
-let name = {
-    firstname: "Sumit",
-    lastname: "Rodrigues"
+// let name = {
+//     firstname: "Sumit",
+//     lastname: "Rodrigues"
+// }
+
+// let printName = function(hometown, state, country) {
+//     console.log( this.firstname + " " + this.lastname + " , " + hometown + " , " + state + " , " + country);
+// }
+
+// let printMyName = printName.bind(name, "Fullerton");
+// printMyName("California", "USA");
+
+// Function.prototype.myBind = function (...args) {
+//     let obj = this;
+//         params = args.slice(1);
+//     return function (...args2) {
+//         obj.apply(args[0], [...params, ...args2]);
+//     }
+// }
+
+// let printMyName2 = printName.myBind(name, "Fullerton");
+// printMyName2("California", "USA");
+
+// Function Currying
+
+let multiply = function (x,y) {
+    console.log(x * y);
 }
 
-let printName = function(hometown, state, country) {
-    console.log( this.firstname + " " + this.lastname + " , " + hometown + " , " + state + " , " + country);
-}
+// below is the simple implementation of how the bind function would work
 
-let printMyName = printName.bind(name, "Fullerton");
-printMyName("California", "USA");
+// let multiplyByTwo = function (y) {
+//     let x = 2;
+//     console.log( x * y);
+// }
 
-Function.prototype.myBind = function (...args) {
-    let obj = this;
-        params = args.slice(1);
-    return function (...args2) {
-        obj.apply(args[0], [...params, ...args2]);
-    }
-}
+// Using Function Closures
 
-let printMyName2 = printName.myBind(name, "Fullerton");
-printMyName2("California", "USA");
+
+let multiplyByTwo = multiply.bind(this, 2, 3); //using bind function to copy the multiply function
+multiplyByTwo(5); // 10
+
+let multiplyByThree = multiply.bind(this, 3); //using bind function to copy the multiply function
+multiplyByThree(5); // 15
