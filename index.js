0313e916497bf9c49a4de7a1711910167abb556c
SumitRodrigues <sumitrod11@gmail.com>
2024-09-28 10:57:56 -0700

    basic implementation of mybind method

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
+// let name = {
+//     firstname: "Sumit",
+//     lastname: "Rodrigues",
+// }
+
+// printFullName = function(hometown, state) {
+//     // debugger; // Pause execution here to inspect
+//     console.log(this.firstname + " " + this.lastname + " from " + hometown + " , " + state);
+// }
+
+// printFullName.call(name, "Alberta" , "GA");
+
+// let name2 = {
+//     firstname: "James",
+//     lastname: "Anderson"
+// }
+
+
+// // Function Borrowing
+// printFullName.call(name2, "Los Angeles", "California");
+
+// printFullName.apply(name2, ["Los Angeles", "California"]);
+
+// // Bind Method
+
+// let printMyName = printFullName.bind(name2, "Los Angeles", "California");
+// console.log(printMyName)
+// printMyName(); // Los Angeles, California James Anderson
+
+//Polyfill for bind method:
+
 let name = {
     firstname: "Sumit",
-    lastname: "Rodrigues",
+    lastname: "Rodrigues"
 }
 
-printFullName = function(hometown, state) {
-    // debugger; // Pause execution here to inspect
-    console.log(this.firstname + " " + this.lastname + " from " + hometown + " , " + state);
+let printName = function() {
+    console.log( this.firstname + " " + this.lastname);
 }
 
-printFullName.call(name, "Alberta" , "GA");
+let printMyName = printName.bind(name);
+printMyName();
 
-let name2 = {
-    firstname: "James",
-    lastname: "Anderson"
+Function.prototype.myBind = function (...args) {
+    let obj = this;
+    return function () {
+        obj.call(args[0]);
+    }
 }
 
-
-// Function Borrowing
-printFullName.call(name2, "Los Angeles", "California");
-
-printFullName.apply(name2, ["Los Angeles", "California"]);
-
-// Bind Method
-
-let printMyName = printFullName.bind(name2, "Los Angeles", "California");
-console.log(printMyName)
-printMyName(); // Los Angeles, California James Anderson
-
+let printMyName2 = printName.myBind(name);
+printMyName2();
