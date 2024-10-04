cd2a9000805180209dc0799baa1d2f28c9d4e566
SumitRodrigues <sumitrod11@gmail.com>
2024-10-03 18:10:22 -0700

    coding questions from github

--- index.js
+++ index.js
@@ -129,3 +129,68 @@
 
 // Event Bubbling
 
+// Coding questions from github
+
+// Question: What is the value of foo?
+var foo = 10 + '20';
+console.log(foo); // "1020"
+
+// Question: What will be the output of the code below?
+console.log(0.1 + 0.2 == 0.3);
+
+// Question: How would you make this work?
+function add(a, b) {
+    // If the second argument 'b' is provided, return the sum of 'a' and 'b'
+    if (b !== undefined) {
+      return a + b;
+    }
+  
+    // If only one argument 'a' is provided, return a new function that takes 'b' as its argument
+    return function(b) {
+      return a + b;
+    };
+  }
+  
+  // Examples
+  console.log(add(2, 5)); // 7
+  console.log(add(2)(5)); // 7
+
+
+  //Question: What value is returned from the following statement?
+  console.log("i'm a lasagna hog".split("").reverse().join(""));
+
+  // Question: What is the value of window.foo?
+  console.log( window.foo || ( window.foo = "bar" ) );
+
+  // Question: What is the outcome of the two alerts below?
+  var foo = "Hello";
+  (function() {
+    var bar = " World";
+    alert(foo + bar);
+  })();
+  alert(foo + bar);
+
+  // Question: What is the value of foo.length?
+  var foo = [];
+  foo.push(1);
+  foo.push(2);
+/* Explanation:
+
+	1.	var foo = [];:
+	•	This line declares a variable foo and initializes it as an empty array.
+	2.	foo.push(1);:
+	•	The push() method adds the element 1 to the end of the array foo.
+	•	Now, the array foo contains one element: [1].
+	•	foo.length will be 1 at this point.
+	3.	foo.push(2);:
+	•	The push() method adds the element 2 to the end of the array foo.
+	•	Now, the array foo contains two elements: [1, 2].
+	•	foo.length will be 2 at this point.
+
+Final Value of foo.length:
+
+The value of foo.length is 2 because there are two elements (1 and 2) in the array after both push() operations.
+
+So, the value of foo.length is:
+
+2. */
